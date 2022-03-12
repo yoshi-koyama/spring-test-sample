953ff3617697eee6305a2edf4ba4cea86b27e486
小山由人 <koyama@reytech.co.jp>
2022-03-12 13:21:09 +0900

    Testを修正。Given-When-Thenでケースを考えるサンプルを用意する

--- src/test/java/com/raisetech/springtestsample/repository/mapper/UserMapperTest.java
+++ src/test/java/com/raisetech/springtestsample/repository/mapper/UserMapperTest.java
@@ -2,14 +2,12 @@ package com.raisetech.springtestsample.repository.mapper;
 
 import com.github.database.rider.core.api.dataset.DataSet;
 import com.github.database.rider.core.api.dataset.ExpectedDataSet;
-import com.github.database.rider.junit5.api.DBRider;
+import com.github.database.rider.spring.api.DBRider;
 import com.raisetech.springtestsample.repository.entity.User;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -24,7 +22,7 @@ class UserMapperTest {
     UserMapper userMapper;
 
     @Test
-    @DataSet(value = "users.yml", cleanBefore = true)
+    @DataSet(value = "users.yml")
     void すべてのユーザーが取得できること() {
         List<User> users = userMapper.findAll();
         assertThat(users).hasSize(2);
@@ -33,7 +31,6 @@ class UserMapperTest {
     @Test
     @DataSet(value = "empty.yml")
     @ExpectedDataSet(value = "expectedAfterInsert.yml", ignoreCols = "id")
-    @Transactional(propagation = Propagation.SUPPORTS)
     void ユーザーの登録ができること() {
         userMapper.save("清水");
         List<User> users = userMapper.findAll();
